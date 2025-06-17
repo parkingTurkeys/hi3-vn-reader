929b35a24eaf9bc0a6e4ceb5399c63e1c796dce3
parking_turkeys <p-t@hackclub.app>
2025-06-17 16:57:30 -0400

    powershelling

--- imgs/get-imgs.ps1
+++ imgs/get-imgs.ps1
@@ -1,3 +1,5 @@
 # src=".*\.png
-$image_urls = @()
-$char_data = Get-Content -Path "..\xml\ae\"
+$char_data = Get-Content -Path "..\xml\ae\character_data.xml"
+$char_data = -split $char_data
+$image_urls = @($char_data -like '*.png')
+echo $char_data
