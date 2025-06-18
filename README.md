fbc5eb4c3dd243ec1f30dd6d4eebecb094fbbe57
parking_turkeys <p-t@hackclub.app>
2025-06-17 23:09:19 -0400

    add some stuff to the html, work on the powershell script, add to README and css

--- README.md
+++ README.md
@@ -13,10 +13,14 @@ it's different from the other ones because it is:
 - useable on mobile
 - __not trying to get the layout of the original__ 
 
-this is because the original layout wouldn't really work too well with mobile and is hard to do. also, it uses plain html/css/js, which means you can load it onto an android device offline using a weird workaround.
+this is because the original layout wouldn't really work too well with mobile and is hard to do. also, this uses plain html/css/js, which means you can load it onto an android device offline using a weird workaround.
 
 ## how to use it on android
 open it as a file:// link instead of a content:// link (so like file:///storage/emulated/0/Download/hi3-vn-reader/index.html)
 
+## progress
+nothing. :3c
+
+
 ## tysms
-i took a lot of inspiration from the [original](https://event.bh3.com/avgAntiEntropy/indexAntiEntropy.php) ofc 
+i took a lot of inspiration from the [original](https://event.bh3.com/avgAntiEntropy/indexAntiEntropy.php) ofc, but i did all the scripting myself! (the original was useful for figuring out what elements i needed though :D)
--- imgs/get-imgs.ps1
+++ imgs/get-imgs.ps1
@@ -1,24 +1,21 @@
 # src=".*\.png
-$char_data = Get-Content -Path "..\xml\ae\character_data.xml"
+$char_data = Get-Content -Path "..\xml\ae\character_data_test.xml"
 $char_data = -split $char_data
 $image_urls = @($char_data -like "src='*.png'")
 $percent_each = 100/$image_urls.length
 $percent = 0
 
+echo $image_urls.length
 for ($i = 0; $i -lt $image_urls.length; $i++) {
-    $percent = ((($i * $percent_each) * 100) - ((($i * $percent_each) * 100) % 1))/100
-    switch ($i % 3) {
-        0 {Write-Progress -Activity "Adding up strings.  " -Status "$percent% Complete:" -PercentComplete $percent}
-        1 {Write-Progress -Activity "Adding up strings.. " -Status "$percent% Complete:" -PercentComplete $percent}
-        2 {Write-Progress -Activity "Adding up strings..." -Status "$percent% Complete:" -PercentComplete $percent}
-    }
+    $percent = $i * $percent_each
+    Write-Progress -Activity "Adding up strings..." -Status "$percent% Complete:" -PercentComplete $percent
     $image_urls[$i] = $image_urls[$i] -split "'"
     $image_urls[$i] = "https://act-webstatic.mihoyo.com/event_bh3_com/avg-anti-entropy/static_CN/resources/chara/" + "$image_urls[$i][1]"
 }
-echo $image_urls
+# echo $image_urls
 
 for ($i = 0; $i -lt $image_urls.length; $i++) {
-    $percent = ((($i * $percent_each) * 100) - ((($i * $percent_each) * 100) % 1))/100
+    $percent = $i * $percent_each
     Write-Progress -Activity "Downloading files..." -Status "$percent% Complete:" -PercentComplete $percent
     $split_url = $image_urls[$i] -split "/"
     $file_name = $split_url[$split_url.length - 1]
