6d8dd90afac0cb9f6e7f1e07fad8610d199b0675
parking_turkeys <p-t@hackclub.app>
2025-06-21 13:29:53 -0400

    added a file where i tried to see if images were real to gitignore, added all 3 quotes to each chapter, checked whether the chapter had the part before trying to add it

--- script.js
+++ script.js
@@ -5,19 +5,26 @@ function generateBoxes(start, end) {
         //for each box
         box_Id = "ch" + i + "-box"
         box_XML_Id = i + 9
-        box_XML_Id = "log" + box_XML_Id + "01"
-        dbg(catalog_list.xml.getElementById(box_XML_Id.attributes))
-        // <div id = 'box_Id'><h2>Chapter i</h2><p><i>Part 1 </i>quotationOne<button
-        box_HTML = "<div id = '" + box_Id + "'><h2>Chapter " + i + "</h2><p><i>Part 1 </i>" + catalog_list.xml.getElementById(box_XML_Id).attributes["quotationOne"].value + "<button onclick = ''>Go</button>" + "</div>"
+        box_XML_Id = "log" + box_XML_Id.toString() + "01"
+        //dbg(catalog_list.xml.getElementById(box_XML_Id).attributes["quotationThree"].value)
+        // <div id = 'box_Id'><h2>Chapter i</h2><p><i>Part 1 </i>quotationOne<button onclick = 'goToScene(partOne)'>Go</button></p><p><i>Part 2 </i>quotationTwo<button onclick = 'goToScene(partTwo)'>Go</button></p><p><i>Part 3 </i>quotationThree<button onclick = 'goToScene(partThree)'>Go</button></div>
+        box_HTML = "<div id = '" + box_Id + "'><h2>Chapter " + i + "</h2><p><i>Part 1 </i>" + catalog_list.xml.getElementById(box_XML_Id).attributes["quotationOne"].value + "<button onclick = 'goToScene(" + catalog_list.xml.getElementById(box_XML_Id).attributes["partOne"].value + ")'>Go</button>"
+        try {box_HTML += "</h2><p><i>Part 2 </i>" + catalog_list.xml.getElementById(box_XML_Id).attributes["quotationTwo"].value + "<button onclick = 'goToScene(" + catalog_list.xml.getElementById(box_XML_Id).attributes["partTwo"].value + ")'>Go</button>" } catch {}
+        try {box_HTML += "</h2><p><i>Part 3 </i>" + catalog_list.xml.getElementById(box_XML_Id).attributes["quotationThree"].value + "<button onclick = 'goToScene(" + catalog_list.xml.getElementById(box_XML_Id).attributes["partThree"].value + ")'>Go</button>" + "</div>"} catch {}
         document.getElementById("chapter-list").appendChild(elementify(box_HTML))
     }
+}
+
+
+function goToScene(ch, scene) {
+    console.log(scene.toString())
 }
 
 
 
 function on_XML_load() {
     dbg("xml loaded!")
-    generateBoxes(1,6)
+    generateBoxes(0,26)
 }
 
 
